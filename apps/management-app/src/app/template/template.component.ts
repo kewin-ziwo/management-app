722ecc0b543ec38e2444e7fbd9d78ab7a53606e3
kewin-ziwo <kewinf271@gmail.com>
2024-06-14 10:39:13 +0400

    layout lib added

--- apps/management-app/src/app/template/template.component.ts
+++ apps/management-app/src/app/template/template.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LayoutComponent } from '@management-app/layout';
 
 @Component({
   selector: 'app-template',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, LayoutComponent],
   templateUrl: './template.component.html',
   styles: `
     :host{
